f5da2fe56bffb4d3c8da99cdf023c3cb598ce2fa
JuanHiguitaR <devjuan06@gmail.com>
2026-05-09 20:56:10 -0400

    bug fixed on PolicyManager

--- src/main/java/service/PolicyManager.java
+++ src/main/java/service/PolicyManager.java
@@ -1,104 +1,140 @@
 package service;
 
+import exception.BussinessRuleException;
 import model.*;
 
+
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class PolicyManager {
     private final List<Insurance> insurances;
+    private final Scanner input = new Scanner(System.in);
+
 
     public PolicyManager(List<Insurance> insurances) {
         this.insurances = insurances;
     }
 
-    public List<Insurance> listAll(){
-        return insurances;
+    public Object listAll(){
+        return insurances.isEmpty() ? "Empty List" : insurances;
     }
 
-    public void newPolicy (String policyType){
-        Scanner input = new Scanner(System.in);
-        String name;
-        int age;
-        double amount;
-        String clientId;
-        String policyId;
-        int carModel;
-        boolean riskZone;
-        switch (policyType.strip()) {
-            case "life" -> {
-                System.out.println("Enter a costumer name: ");
-                name = input.nextLine().strip();
-                input.nextLine();
-                System.out.println("Enter the costumer's age: ");
-                age = input.nextInt();
-                input.nextLine();
-                System.out.println("Enter a insurance life amount (USD): ");
-                amount = input.nextDouble();
-                input.nextLine();
-                clientId = generateNewId();
-                policyId = generateNewPolicyId();
-                insurances.add(new LifeInsurance(policyId, new Client(clientId, name, age), amount));
-                System.out.println("Life Policy: " + policyId + " Added Successful");
-            }
-            case "car" -> {
-                System.out.println("Enter a costumer name: ");
-                name = input.nextLine().strip();
-                input.nextLine();
-                System.out.println("Enter the costumer's age: ");
-                age = input.nextInt();
-                input.nextLine();
-                System.out.println("Enter the car model: ");
-                carModel = input.nextInt();
-                input.nextLine();
-                System.out.println("Enter a insurance car amount (USD): ");
-                amount = input.nextDouble();
-                input.nextLine();
-                clientId = generateNewId();
-                policyId = generateNewPolicyId();
-                insurances.add(new CarInsurance(policyId, new Client(clientId, name, age),
-                        amount, carModel));
-                System.out.println("Car Policy: " + policyId + " Added Successful");
-            }
-            case "house" -> {
-                System.out.println("Enter a costumer name: ");
-                name = input.nextLine().strip();
-                input.nextLine();
-                System.out.println("Enter the costumer's age: ");
-                age = input.nextInt();
-                input.nextLine();
-                System.out.println("Enter the house risk zone: ");
-                riskZone = input.nextBoolean();
-                input.nextLine();
-                System.out.println("Enter a insurance car amount (USD): ");
-                amount = input.nextDouble();
-                input.nextLine();
-                clientId = generateNewId();
-                policyId = generateNewPolicyId();
-                insurances.add(new HomeInsurance(policyId, new Client(clientId, name, age),
-                        amount, riskZone));
-            }
-            default -> System.out.println("Wrong insurance type!");
+    public void newPolicy (String policyType) {
+
+        String type = policyType.strip().toUpperCase(Locale.ROOT);
+
+        System.out.println("Enter a Customer name: ");
+        String name = input.nextLine().strip();
+        input.nextLine();
+        int age = promptInt("Enter the customer's age: ",
+                "The age client couldn't be negative or zero");
+
+        Client client = new Client(getNewClientId(), name, age);
+
+        Insurance insurance = switch (type){
+            case "LIFE" -> createLifeInsurance(client);
+            case "CAR" -> createCarInsurance(client);
+            case "HOME" -> createHomeInsurance(client);
+            default -> null;
+        };
+
+        if(insurance != null){
+            insurances.add(insurance);
+            System.out.println("The Policy :" + insurance.policyId() + " Added Successful");
+        }else{
+            throw new BussinessRuleException("Wrong insurance type");
         }
     }
 
-    private String generateNewId(){
-        String lastClientId = insurances.stream()
-                .map(id -> id.client().clientId())
-                .max(String::compareTo)
-                .orElse("C000");
-        int newID = Integer.parseInt(lastClientId.substring(1)) + 1;
-        return String.format("C%03d", newID);
+    public List<Insurance> findPolicyById(String clientId){
+        return insurances.stream()
+                .filter(p -> p.client().clientId().equals(clientId))
+                .toList();
+    }
+    private String getNewClientId(){
+        String clientId = getLastClientId();
+
+        if(!clientId.equals("C000")){
+            System.out.println("The last customer id: " + clientId +
+                    "Enter a consecutive customer id: ");
+            clientId = input.nextLine();
+            input.nextLine();
+        }
+        return clientId;
     }
 
-    private String generateNewPolicyId(){
-        String lastClientId = insurances.stream()
+    private String getNewPolicyId(){
+        String policyId = getLastPolicyId();
+
+        if(!policyId.equals("POL-000")){
+            System.out.println("The last policy id: " + policyId +
+                    "Enter a consecutive policy id: ");
+            policyId = input.nextLine();
+            input.nextLine();
+        }
+        return policyId;
+    }
+
+    private String getLastClientId(){
+        return insurances.stream()
+                .map(c -> c.client().clientId())
+                .max(String::compareTo)
+                .orElse("C000");
+    }
+
+    private String getLastPolicyId(){
+        return insurances.stream()
                 .map(Insurance::policyId)
                 .max(String::compareTo)
                 .orElse("POL-000");
-        int newID = Integer.parseInt(lastClientId.replace("POL-", "").strip()) + 1;
-        return String.format("POL-%03d", newID);
     }
 
+    private int promptInt(String message, String errorMessage){
+        System.out.println(message);
+        int value = input.nextInt();
+        input.nextLine();
+        if(errorMessage != null && value <= 0){
+            throw new BussinessRuleException(errorMessage);
+        }
+        return value;
+    }
 
+    private Double promptDouble(String message, String errorMessage){
+        System.out.println(message);
+        double value = input.nextDouble();
+        input.nextLine();
+        if(errorMessage != null && value <= 0.0){
+            throw new BussinessRuleException(errorMessage);
+        }
+        return value;
+    }
+
+    private LifeInsurance createLifeInsurance(Client client){
+        double amount = promptDouble("Enter a life insurance amount (USD): ",
+                "The policy amount couldn't be negative or zero");
+
+        return new LifeInsurance(getNewPolicyId(), client, amount);
+    }
+
+    private CarInsurance createCarInsurance(Client client){
+        double amount = promptDouble("Enter a car insurance amount (USD): ",
+                "The policy amount couldn't be negative or zero");
+
+        int carModel = promptInt("Enter the car model (year): ", null);
+
+        return new CarInsurance(getNewPolicyId(), client, amount, carModel);
+    }
+
+    private HomeInsurance createHomeInsurance(Client client){
+        double amount = promptDouble("Enter a home insurance amount (USD): ",
+                "The policy amount couldn't be negative or zero");
+
+        System.out.println("Enter the house risk zone (true/false): ");
+        boolean riskZone = input.nextBoolean();
+        input.nextLine();
+
+        return new HomeInsurance(getNewPolicyId(), client, amount, riskZone);
+    }
 }
